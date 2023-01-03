886b225840cc61ce423dec8c60313da265e49652
itezjunhyeok <junhyeok@itez.io>
2023-01-04 03:00:58 +0900

    로그인 초안

--- app.py
+++ app.py
@@ -2,15 +2,21 @@
 #
 import os
 import pymysql # mysql을 python에서 사용할 시 추가
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 # from models import Member #일단 sqlalchemy 사용 안함
 
 app = Flask(__name__)
+# secret_key: 서버상에 동작하는 어플리케이션을 구분하기 위해 사용
+app.secret_key = os.urandom(24)
 
 # 라우팅: route() 데코레이터는 Flask에서 URL 방문할 때 준비된 함수가 트리거되도록 바인딩
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method == 'POST':
+        session.pop('id', None) # 세션 삭제
+        return redirect('/')
 
 # route() 데코레이터의 methods 인수로 POST를 지정해서 POST요청도 처리
 @app.route('/sign-up', methods=['GET', 'POST'])
@@ -51,9 +57,43 @@ def signUp():
         return redirect('/')
         
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    # 요청 메소드가 GET일 때
+    if request.method == 'GET':
+        # login html파일을 렌더링
+        return render_template("login.html")
+    # 요청 메소드가 POST일 때
+    elif request.method == 'POST':
+        id = request.form.get('id')
+        pw = request.form.get('pw')
+        
+        if len(id) == 0 or len(pw) == 0:
+            return '입력되지 않은 정보가 있습니다.'
+        else:
+            # MySQL 연결
+            conn = pymysql.connect(host='127.0.0.1', user='root', password='admin1234', db='mydb', charset='utf8')
+            # 커서 객체 생성 (커서 객체에 DB작업을 위한 함수들이 포함)
+            cur = conn.cursor()
+            # 실행할 SQL문 정의
+            sql = '''
+            select * from member
+            where id=%s
+            and pw=%s
+            '''
+            vals = (id, pw)
+            cur.execute(sql, vals)
+            res = cur.fetchall()
+            session['id'] = res[0]
+
+            # (1, 'itez', '1234', 'itezitez', 'no')
+            # 그냥 print 해보는 용도(값 확인용)로 삭제 예정
+            for i in cur:
+                print(i)
+            print(session['id'])
+
+            return redirect('/')
+
 
 @app.route('/board')
 def board():
